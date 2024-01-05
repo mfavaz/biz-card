0c26f2f085673fd04739b653cac745ddead1d807
Mohammed Favas K K <mfavas.mf@gmail.com>
2024-01-06 02:20:16 +0530

    footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -12,26 +12,8 @@
       </p> -->
       <p class="font-extrabold text-xl mt-2 leading-tight">EnBizCard</p>
     </div>
-    <p class="mt-6 text-center text-gray-400">
-      Made with ❤️ by
-      <a
-        class="underline text-emerald-600 font-extrabold hover:text-emerald-500 focus:text-emerald-500 transition-colors duration-200"
-        href="https://www.vishnuraghav.com/"
-        target="_blank"
-        rel="noopener noreferrer"
-        >Vishnu&nbsp;Raghav</a
-      >
-    </p>
-    <p class="mt-1 mb-8 text-center text-xs text-gray-400">
-      <a
-        class="underline text-emerald-600 font-extrabold hover:text-emerald-500 focus:text-emerald-500 transition-colors duration-200"
-        href="https://github.com/vishnuraghavb/EnBizCard"
-        target="_blank"
-        rel="noopener noreferrer"
-        >View&nbsp;Source</a
-      >
-      | AGPLv3 License
-    </p>
+    
+    
   </footer>
 </template>
 
